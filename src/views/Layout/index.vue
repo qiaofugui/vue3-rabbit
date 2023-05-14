1fafa4977307084193f50bccc3c428525f41e800
qiaofugui <903111082@qq.com>
2023-05-14 15:57:52 +0800

    layout 静态结构搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,5 +1,12 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
+
 <template>
-  Layout
-  <!-- 二级路由出口 -->
-  <router-view />
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
 </template>
