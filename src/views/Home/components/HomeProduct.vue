c58812daa67b94d6eaeb12ecbb84c9b9a8e2fc90
qiaofugui <903111082@qq.com>
2023-05-14 21:18:40 +0800

    home GoodsItem 组件封装

--- src/views/Home/components/HomeProduct.vue
+++ src/views/Home/components/HomeProduct.vue
@@ -2,6 +2,7 @@
 import HomePanel from './HomePanel.vue'
 import { getGoodsAPI } from '@/apis/home.js'
 import { onMounted, ref } from 'vue'
+import GoodsItem from './GoodsItem.vue'
 
 const goodsProduct = ref([])
 const getGoods = async () => {
@@ -38,18 +39,7 @@ onMounted(() => {
             v-for="good in cate.goods"
             :key="good.id"
           >
-            <RouterLink
-              to="/"
-              class="goods-item"
-            >
-              <img
-                v-img-lazy="good.picture"
-                alt=""
-              />
-              <p class="name ellipsis">{{ good.name }}</p>
-              <p class="desc ellipsis">{{ good.desc }}</p>
-              <p class="price">&yen;{{ good.price }}</p>
-            </RouterLink>
+            <GoodsItem :good="good"/>
           </li>
         </ul>
       </div>
@@ -146,42 +136,6 @@ onMounted(() => {
         &:nth-child(4n) {
           margin-right: 0;
         }
-      }
-    }
-
-    .goods-item {
-      display: block;
-      width: 220px;
-      padding: 20px 30px;
-      text-align: center;
-      transition: all .5s;
-
-      &:hover {
-        transform: translate3d(0, -3px, 0);
-        box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
-      }
-
-      img {
-        width: 160px;
-        height: 160px;
-      }
-
-      p {
-        padding-top: 10px;
-      }
-
-      .name {
-        font-size: 16px;
-      }
-
-      .desc {
-        color: #999;
-        height: 29px;
-      }
-
-      .price {
-        color: $priceColor;
-        font-size: 20px;
       }
     }
   }
