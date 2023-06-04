6ec78c70f7888868ac8fee3e3080e0ab5acbbfdb
qiaoronggui <qiaoronggui@qiyu3.onaliyun.com>
2023-06-04 18:28:10 +0800

    结算页 地址切换 打开弹窗

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -15,6 +15,9 @@ const getCheckInfo = async () => {
 }
 onMounted(() => getCheckInfo())
 
+// 控制切换地址弹窗的显示隐藏
+const toggleFlag = ref(false)
+
 </script>
 
 <template>
@@ -150,7 +153,35 @@ onMounted(() => getCheckInfo())
       </div>
     </div>
   </div>
+
   <!-- 切换地址 -->
+  <el-dialog
+    v-model="toggleFlag"
+    title="切换收货地址"
+    width="30%"
+    center
+  >
+    <div class="addressWrapper">
+      <div
+        class="text item"
+        v-for="item in checkInfo.userAddresses"
+        :key="item.id"
+      >
+        <ul>
+          <li><span>收<i />货<i />人：</span>{{ item.receiver }} </li>
+          <li><span>联系方式：</span>{{ item.contact }}</li>
+          <li><span>收货地址：</span>{{ item.fullLocation + item.address }}</li>
+        </ul>
+      </div>
+    </div>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button>取消</el-button>
+        <el-button type="primary">确定</el-button>
+      </span>
+    </template>
+  </el-dialog>
+
   <!-- 添加地址 -->
 </template>
 
