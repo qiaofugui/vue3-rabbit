2dfba33ea95438dc22092080473771d40bb8363c
qiaoronggui <qiaoronggui@qiyu3.onaliyun.com>
2023-06-04 20:04:55 +0800

    会员中心 基础配置

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,9 @@ import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
 import PayBack from '@/views/Pay/PayBack.vue'
+import Member from '@/views/Member/index.vue'
+import MemberInfo from '@/views/Member/components/UserInfo.vue'
+import MemberOrder from '@/views/Member/components/UserOrder.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -66,6 +69,24 @@ const router = createRouter({
           path: 'paycallback',
           name: 'PayBack',
           component: PayBack
+        },
+        {
+          path: 'member',
+          name: 'Member',
+          component: Member,
+          redirect: '/member/user',
+          children: [
+            {
+              path: 'user',
+              name: 'MemberInfo',
+              component: MemberInfo
+            },
+            {
+              path: 'order',
+              name: 'MemberOrder',
+              component: MemberOrder
+            }
+          ]
         },
       ]
     },
--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -41,8 +41,14 @@ const logout = () => {
               </template>
             </el-popconfirm>
           </li>
-          <li><a href="javascript:;">我的订单</a></li>
-          <li><a href="javascript:;">会员中心</a></li>
+          <li><a
+              href="javascript:;"
+              @click="$router.push('/member/order')"
+            >我的订单</a></li>
+          <li><a
+              href="javascript:;"
+              @click="$router.push('/member/user')"
+            >会员中心</a></li>
         </template>
         <template v-else>
           <li><a
